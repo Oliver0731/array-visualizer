fd08f1aac558b2b9faf9e3498e2a855458d79e70
Oliver0731 <o.townell@share.epsb.ca>
2024-02-26 09:59:35 -0700

    update.js/css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 // Array Visualizer Start
 
 // Global Variables
-let data = [1, 2, 3, 5, 8];
+let data = [1, 2, 3, 5, 8, 7, 4, 1, 0, 9, 10, 7];
 let maxVal = 10; // max data value
+
+
+//HTML Variables
+let outputEl = document.getElementById("container");
+
+//Traverse to Display Data Array
+function drawDataArray() {
+    let outputStr = "";
+    for (let i = 0; i < data.length; i++){
+outputStr += `<div>${data[i]}<div>`
+    }
+    // for (let val of data){
+    //     outputStr += `<div>${val}<div>`
+    //         }
+    outputEl.innerHTML = outputStr;
+}
+
+drawDataArray();
+
+
