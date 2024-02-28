0944bf78172f98acd3b6e69afa427fa398ab15ba
Oliver0731 <o.townell@share.epsb.ca>
2024-02-28 10:00:24 -0700

    update

--- js/main.js
+++ js/main.js
@@ -4,22 +4,28 @@
 let data = [1, 2, 3, 5, 8, 7, 4, 1, 0, 9, 10, 7];
 let maxVal = 10; // max data value
 
-
 //HTML Variables
 let outputEl = document.getElementById("container");
 
 //Traverse to Display Data Array
 function drawDataArray() {
-    let outputStr = "";
-    for (let i = 0; i < data.length; i++){
-outputStr += `<div>${data[i]}<div>`
-    }
-    // for (let val of data){
-    //     outputStr += `<div>${val}<div>`
-    //         }
-    outputEl.innerHTML = outputStr;
+  let outputStr = "";
+  for (let val of data) {
+    let divHeight = (val / maxVal) * 600;
+    outputStr += `<div style="height:${divHeight}px">${val}</div>`;
+  }
+  // for (let val of data){
+  //     outputStr += `<div>${val}<div>`
+  //         }
+  outputEl.innerHTML = outputStr;
 }
 
-drawDataArray();
+document.getElementById("container").addEventListener("click", killBar);
 
-
+function killBar() {
+  drawDataArray();
+  console.log("splice");
+  let numAdd = 1;
+  data.push(numAdd);
+  numAdd += 1;
+}
